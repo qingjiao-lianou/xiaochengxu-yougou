64017707c6c5a7421288a88be9db2459e49f5724
蟑螂恶霸 <zhanglangeba@qq.com>
2019-11-27 16:40:22 +0800

    搜索功能完成

--- pages/search/index.js
+++ pages/search/index.js
@@ -10,15 +10,42 @@ Page({
    * 页面的初始数据
    */
   data: {
+    list: [],
+    isShow: false,
+    valueIup: ''
+  },
+
+  setTime: -1,
+  // 绑定输入框事件  获取关键字
+  handleInput(e) {
+    const { value } = e.detail
+    if (value.trim()) {
+      this.setData({
+        isShow: true
+      })
+      clearTimeout(this.setTime)
+      this.setTime = setTimeout(() => {
+        this.getQuery(value)
+      }, 1000)
+    }
 
   },
 
-  // 绑定输入框事件
-  handleInput(e){
-     console.log(e);
-     const {value} = e.dedail
-     
+  // 获取搜索内容
+  async getQuery(value) {
+    const res = await request({ url: '/goods/qsearch', data: { query: value } })
+    this.setData({
+      list: res
+    })
+
+  },
+
+  handleBtn() {
+    this.setData({
+      list: [],
+      isShow: false,
+      valueIup: ''
+    })
+
   }
-
-
 })
